b6fda2f03da38a60e158d0b7f67e3ccb6eed4aba
brenda <brendamakena09@gmail.com>
2023-11-24 11:14:03 +0300

    assignment completed

---
+++ lib/example.py
@@ -0,0 +1,38 @@
+# def divide(num1, num2):
+#     try:
+#         quotient = num1 / num2
+#         print(quotient)
+#     except ZeroDivisionError:
+#         print("Error: num2 cannot be equal to 0")
+#     except TypeError:
+#         print("Error: input must be of type int or float")
+#     finally:
+#         print("Isn't division fun?")
+#         divide()
+        
+        
+#  dog = "cuddly"
+
+# if dog == "hungry":
+#     owner = "Refilling food bowl."
+# elif dog == "thirsty":
+#     owner = "Refilling water bowl."
+# elif dog == "playful":
+#     owner = "Playing tug-of-war."
+# elif dog == "cuddly":
+#     owner = "Snuggling."
+# else:
+#     owner = "Reading newspaper."
+    
+# dog = "cuddly"
+
+# dict_map = {
+#     "hungry": "Refilling food bowl.",
+#     "thirsty": "Refilling water bowl.",
+#     "playful": "Playing tug-of-war.",
+#     "cuddly": "Snuggling.",
+# }
+
+# # Remember that a dictionary's .get() method lets us set a default value!
+# owner = dict_map.get(dog, "Reading newspaper.")
+       
